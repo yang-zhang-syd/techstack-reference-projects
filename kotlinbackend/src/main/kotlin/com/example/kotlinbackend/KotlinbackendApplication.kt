167c0f7dcd0c3f7bdd439f3a62131c1ad9c1bf01
Yang Zhang <yangzhang@Yangs-MacBook-Pro.local>
2022-09-06 22:07:15 +1000

    Added deployment.yaml to kotlin backend

--- kotlinbackend/src/main/kotlin/com/example/kotlinbackend/KotlinbackendApplication.kt
+++ kotlinbackend/src/main/kotlin/com/example/kotlinbackend/KotlinbackendApplication.kt
@@ -4,9 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 @SpringBootApplication
 class KotlinBackendApplication
@@ -19,23 +16,4 @@ fun main(args: Array<String>) {
 class MessageResource() {
 	@GetMapping
 	fun index(): String = "Hello Kotlin!"
-}
-
-
-
-private suspend fun loadItemsFromDB(): List<String> {
-	delay(5000)
-	return listOf("Kotlin", "Java", "C#")
-}
-
-private suspend fun loadItemsFromNetwork(): List<String> {
-	delay(5000)
-	return listOf("Rust", "Python", "C")
-}
-
-// Use async() to parallelize the execution of the two provided suspend functions
-// Use await() to get results of both async() calls
-// Convert the results into a unified list and print out to the console
-fun main(): Unit = runBlocking {
-
 }
--- kotlinbackend/src/main/kotlin/com/example/kotlinbackend/StringResult.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinbackend
-
-sealed class StringResult {
-    class Success(val result: String) : StringResult()
-
-    class Error(val error: Throwable) : StringResult()
-}
